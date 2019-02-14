e9676bffceb788839bfc6106eb075f115289ad24
CodyvanderPoel <cvanderpoel@basecampcodingacademy.org>
2019-02-14 13:05:50 -0600

    This file creates all of the tables at once

---
+++ schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS albums (
+    id SERIAL PRIMARY KEY,
+    title TEXT,
+    artist TEXT,
+    label TEXT,
+    genre TEXT
+);
+
+CREATE TABLE IF NOT EXISTS reviewers (
+    id SERIAL PRIMARY KEY,
+    username TEXT,
+    type_of_user TEXT
+);
+
+CREATE TABLE IF NOT EXISTS reviews (
+    id INTEGER PRIMARY KEY,
+    url TEXT,
+    reviewerId INTEGER REFERENCES reviewers(id),
+    albumId INTEGER REFERENCES albums(id),
+    release_date DATE,
+    score FLOAT,
+    is_best_music BOOLEAN
+);
