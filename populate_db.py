eb9dcf70e73ecbe90ef59e9607fdbf8da3cf514a
CodyvanderPoel <cvanderpoel@basecampcodingacademy.org>
2019-02-14 14:45:34 -0600

    Began making changes so it would add the files properly

--- populate_db.py
+++ populate_db.py
@@ -5,8 +5,30 @@ reviews = load(open('./reviews.json'))
 
 db = Database('postgres:///pitchfork-reviews')
 
+i = 0
 for review in reviews:
     album = review['album']
     album_id = db.query_file('./insert_album.sql', **album).first().id
+    reviewer = review['author']
+    reviewer_id = db.query_file('./insert_reviewer.sql', username=reviewer['name'], ).first().id
+    idn = review['id']
+    url = review['url']
+    date = review['date']
+    year = date['year']
+    month = date['month']
+    day = date['day']
+    release_date = f'{year}-{month}-{day}'
+    score = review['score']
+    ibm = review['is_best_new_music']
+    i += 1
+    print(i)
 
-    db.query_file("insert_review.sql", album_id=album_id)
+    db.query_file(
+        "insert_review.sql",
+        id=idn,
+        url=url,
+        reviewer_id=reviewer_id,
+        album_id=album_id,
+        release_date=release_date,
+        score=score,
+        is_best_music=ibm)
